00c8e73c325579ef30cb3b837281dfb6d68eeaa5
Stephan Seifermann <seiferma@users.noreply.github.com>
2017-12-29 10:47:58 +0100

    Bugfixes.

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 SCRIPT_FILE=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
 
-if [ ! -f "$PREFERENCES" ]; then
+PREF_DIR=$PLEX_HOME/Application\ Support/Plex\ Media\ Server
+PREF_FILE=Preferences.xml
+if [ ! -f "$PREF_DIR/$PREF_FILE" ]; then
     echo Initializing preferences.
-    mkdir -p $PLEX_HOME/Application\ Support/Plex\ Media\ Server
-    cp $SCRIPT_DIR/Preferences.xml $PLEX_HOME/Application\ Support/Plex\ Media\ Server/Preferences.xml
+    mkdir -p "$PREF_DIR"
+    cp $SCRIPT_DIR/Preferences.xml "$PREF_DIR/$PREF_FILE"
 fi
 LD_LIBRARY_PATH=/usr/lib/plexmediaserver "/usr/lib/plexmediaserver/Plex Media Server"
