05e8b683fb1e215f512d428fb902aee6a052e41b
Stephan Seifermann <seiferma@users.noreply.github.com>
2017-12-29 01:46:43 +0100

    Added default preferences.

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+SCRIPT_FILE=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
+
+if [ ! -f "$PREFERENCES" ]; then
+    echo Initializing preferences.
+    mkdir -p $PLEX_HOME/Application\ Support/Plex\ Media\ Server
+    cp $SCRIPT_DIR/Preferences.xml $PLEX_HOME/Application\ Support/Plex\ Media\ Server/Preferences.xml
+fi
 LD_LIBRARY_PATH=/usr/lib/plexmediaserver "/usr/lib/plexmediaserver/Plex Media Server"
